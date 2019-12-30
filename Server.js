e76ecabec412183e975a0d8ab529203f3eb91eba
namshiynwa <59202574+namshiynwa@users.noreply.github.com>
2019-12-30 15:57:23 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname ));
 
 app.get('/*', function(req,res) {
 
-res.sendFile('/index.html');
+res.sendFile('dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
